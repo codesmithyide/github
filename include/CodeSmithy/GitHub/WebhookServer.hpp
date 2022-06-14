56f850eedb100c13caf8c7ea39023be4abd29ab0
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-06-14 14:05:56 +0100

    Add files

---
+++ include/CodeSmithy/GitHub/WebhookServer.hpp
@@ -0,0 +1,11 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    All rights reserved.
+*/
+
+#ifndef _CODESMITHY_GITHUB_WEBHOOKSERVER_HPP_
+#define _CODESMITHY_GITHUB_WEBHOOKSERVER_HPP_
+
+#include "WebhookServer/GithubWebhookServer.hpp"
+
+#endif
